cf66888dd0be65fef4fe73a72a9ef93f48d54643
unknown <saidaitsalm@gmail.com>
2019-12-15 22:44:04 +0100

    Initial Commit

--- DatingApp-SPA/src/app/Value/Value.component.ts
+++ DatingApp-SPA/src/app/Value/Value.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 
+
 @Component({
   // tslint:disable-next-line: component-selector
   selector: 'app-Value',
