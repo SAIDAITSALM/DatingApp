2171ec08223d3812c835b4bb6790c6d317b07f2b
unknown <saidaitsalm@gmail.com>
2020-02-07 23:39:22 +0100

    commit

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {map} from 'rxjs/operators';
+import {JwtHelperService} from '@auth0/angular-jwt';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-baseUrl = 'http://localhost:5000/api/auth/';
-constructor(private http: HttpClient) { }
+   baseUrl = 'http://localhost:5000/api/auth/';
+   jwtHelper = new JwtHelperService();
+   decodedToken: any;
+
+constructor(private http: HttpClient) {}
+
 Login(model: any) {
   return this.http.post(this.baseUrl + 'Login', model)
   .pipe(
@@ -14,6 +19,8 @@ Login(model: any) {
       const user = response;
       if(user) {
         localStorage.setItem('token', user.token);
+        this.decodedToken = this.jwtHelper.decodeToken(user.token);
+        console.log(this.decodedToken);
       }
     })
   );
@@ -21,5 +28,9 @@ Login(model: any) {
   Register(model: any) {
     return this.http.post(this.baseUrl + 'register', model);
 
+  }
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !this.jwtHelper.isTokenExpired(token);
   }
 }
